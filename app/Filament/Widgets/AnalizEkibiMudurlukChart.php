1a10d77ffc5582fc6763833e7421941627a0be89
Çağrı El <cagriel@MacBook-Pro.local>
2026-05-13 08:32:23 +0300

    fix

--- app/Filament/Widgets/AnalizEkibiMudurlukChart.php
+++ app/Filament/Widgets/AnalizEkibiMudurlukChart.php
@@ -9,7 +9,7 @@ use Filament\Widgets\ChartWidget;
 
 class AnalizEkibiMudurlukChart extends ChartWidget
 {
-    protected static ?string $heading = 'Bağlı Müdürlüklerde Aylık İş Yoğunluğu';
+    protected static ?string $heading = 'Müdürlüklerde Aylık İş Yoğunluğu';
 
     protected int|string|array $columnSpan = 'full';
 
@@ -54,8 +54,7 @@ class AnalizEkibiMudurlukChart extends ChartWidget
         $month = max(1, min(12, (int) $monthRaw));
         $monthVariants = [(string) $month, str_pad((string) $month, 2, '0', STR_PAD_LEFT)];
 
-        $directorates = $user->assignedDirectorates()
-            ->orderBy('users.name')
+        $directorates = User::queryMudurlukReportingAccounts()
             ->get(['users.id', 'users.name']);
 
         $labels = [];
